490c4d316a0077f5ba1a0e4ea2bc1706160f84b0
Abhijit Sarkar <106448232+abhijit10601@users.noreply.github.com>
2025-05-02 08:46:30 +0530

    Create 20_NthFibonacci.cpp

---
+++ 1. DSA Foundation/20_NthFibonacci.cpp
@@ -0,0 +1,27 @@
+/*
+Problem Description
+Given an integer n, you have to find the nth fibonacci number. The fibonacci sequence is given by 0,1,1,2,3,5,8,... where 0 and 1 are the 0th and 1st fibonacci numbers respectively and every consecutive number is the sum of the previous two numbers in the sequence.
+
+Input format
+There is one line of input, containing an integer n.
+
+Output format
+Print the nth fibonacci number.
+
+Sample Input 1
+3
+
+Sample Output 1
+2
+
+Explanation 1
+3rd fibonacci number is equal to the sum of 1st and 2nd fibonacci numbers i.e. 1 + 1 = 2.
+
+Constraints
+0 <= n <= 20
+*/
+int nthFibonacciNumber(int n){
+      if(n==0||n==1)return n;
+
+      return nthFibonacciNumber(n-2)+nthFibonacciNumber(n-1);
+}
