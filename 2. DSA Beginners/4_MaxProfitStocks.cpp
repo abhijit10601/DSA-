a3e258eb9f3da34962bb391a59827b99bdba522d
Abhijit Sarkar <106448232+abhijit10601@users.noreply.github.com>
2025-09-21 12:09:26 +0530

    Update 4_MaxProfitStocks.cpp

--- 2. DSA Beginners/4_MaxProfitStocks.cpp
+++ 2. DSA Beginners/4_MaxProfitStocks.cpp
@@ -62,12 +62,10 @@ No profit is possible due to continuously decreasing prices, so the maximum prof
         int maxProfit(vector<int>& prices) {
             int profit = 0;
 
-
-            for(int i=0;i<prices.size()-1;i++){
-              if(prices[i]<prices[i+1]){
-                profit+=abs(prices[i]-prices[i+1]);
-              }
-            }
+           // multiple transaction --> absolute difference between all elements 
+           for(int i=1;i<prices.size();i++){
+             if(prices[i]>prices[i-1])profit+=prices[i]-prices[i-1];
+           }
 
             return profit;
         }
