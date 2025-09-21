dc8a7b52a871aabdd346c052e24b47cf9e1e003d
Abhijit Sarkar <106448232+abhijit10601@users.noreply.github.com>
2025-09-21 13:24:53 +0530

    Update 17_Minimum_Number_Of_Meeting_Rooms_Required.cpp

--- 2. DSA Beginners/17_Minimum_Number_Of_Meeting_Rooms_Required.cpp
+++ 2. DSA Beginners/17_Minimum_Number_Of_Meeting_Rooms_Required.cpp
@@ -48,5 +48,28 @@ int meetingRooms(int n, vector<vector<int>>& time){
    }
 return minHeap.size();
 
-
 }
+
+
+
+//OPTIMAL
+int maxMeetings(vector<int>& start, vector<int>& end) {
+        // Your code here
+        int count=0;
+        int lastFinish=-1;
+        vector<pair<int,int>> meeting;
+        int n=start.size();
+        for(int i=0;i<n;i++){ //n
+            meeting.push_back({end[i],start[i]});
+        }
+        sort(meeting.begin(),meeting.end());  // nlogn
+        
+        for(int i=0;i<n;i++){
+            if(meeting[i].second>lastFinish){ // curr start time > last finish time
+            count++;
+            lastFinish=meeting[i].first; // curr finish/end time
+                
+            }
+        }
+        return count;
+    }
